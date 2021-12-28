2e1d1ac2c506fc4b12ad3de3ebc6a33341635037
Stefan van Delft <stefan.van.delft@bettyblocks.com>
2021-12-28 14:37:34 +0100

    Make install script ready to be used.

--- install.sh
+++ install.sh
@@ -1,3 +1,67 @@
 #/bin/bash
 
-#todo create script for creating system links
+ZSHRC="$HOME/.zshrc"
+
+get_installer() {
+if [ $SYSTEM == "Linux" ];
+then 
+  INSTALLER="sudo apt get";
+else 
+  # not_supported;
+  INSTALLER="brew install";
+fi;
+
+}
+
+print() {
+  echo " - $1\n"
+}
+
+install_linux() {
+
+  print "Installing following packages:"
+  echo "$(cat -n pkglist) \n"
+  sudo apt-get install $(cat pkglist)
+
+  print "Installing zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  echo "...and its plugins!\n"
+  git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+  echo "... enabling plugins."
+  sed -i 's/plugin=(git)/plugin=(zsh-syntax-highlighting zsh-autosuggestions colored-man-pages git shrink-path fzf)/' $ZSHRC
+
+  print "Creating links for dotfiles"
+  ln -sv .gitconfig $HOME
+  ln -sv .alias $HOME
+  ln -sv .env_vars $HOME
+
+  # Append settings to .zsh file
+  echo "# Appended by install script." >> $ZSHRC
+
+  print "Env vars are managed by .zshrc file."
+  echo "if [ -f $HOME/.env_vars ]; then source $HOME/.env_vars; fi" >> $ZSHRC
+
+  print "Add  aliases to .zshrc file."
+  echo "if [ -f $HOME/.alias ]; then source $HOME.alias; fi" >> $ZSHRC
+
+}
+
+not_supported() {
+echo "Only Linux is supported at this moment."
+exit 1
+}
+
+SYSTEM="$(uname)"
+get_installer
+echo $INSTALLER
+
+if [ $SYSTEM == "Linux" ];
+then 
+  install_linux;
+else 
+  not_supported;
+fi;
+
