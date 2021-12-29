4cb628f6830fd38956f6059582ac1b6e35894101
Stefan van Delft <stefan@pop-os.localdomain>
2021-12-29 21:50:40 +0100

    Add the ability to install docker and slack. Also ability to configure nvim.

--- install.sh
+++ install.sh
@@ -14,27 +14,77 @@ fi;
 }
 
 print() {
-  echo " - $1\n"
+  echo -e "\n - $1\n"
+}
+
+configure_nvim() {
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  	ln -sv $PWD/.vimrc $HOME/.nvimrc
+	nvim --cmd PluginInstall --cmd qall
+}
+
+install_docker() {
+	curl -fsSL https://get.docker.com -o get-docker.sh
+ 	sudo sh get-docker.sh
 }
 
 install_linux() {
+  print "Please enter gitconfigs:"
+
+  # Set git config correct
+  read -p "Enter email that will be used mainly for git: " gitemail
+  read -p "Enter name: " gitname
+
+  print "Configure nvim? [y/n]"
+  read nvim_consent
+
+  print "Install docker?[y/n] info: https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script?"
+  read docker_consent
+
+  print "Install slack? [y/n]"
+  read slack_consent
 
   print "Installing following packages:"
-  echo "$(cat -n pkglist) \n"
+  echo "$(cat -n pkglist) "
   sudo apt-get install $(cat pkglist)
+
+  if [[ $docker_consent == "Y" || $docker_consent == "y" ]]; then
+	  print "Installing Docker."
+	  install_docker
+	  echo "OK."
+  else
+	  print "Skipping Docker."
+  fi
+
+  if [[ $slack_consent == "Y" || $slack_consent == "y" ]]; then
+	  print "Installing Slack."
+	  sudo apt-get install slack-desktop
+	  echo "OK."
+  else
+	  print "Skipping Slack."
+  fi
 
   print "Installing zsh"
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-  echo "...and its plugins!\n"
+  echo "...and its plugins!"
   git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
   echo "... enabling plugins."
-  sed -i 's/plugin=(git)/plugin=(zsh-syntax-highlighting zsh-autosuggestions colored-man-pages git shrink-path fzf)/' $ZSHRC
+  sed -i 's/plugins=(git)/plugins=(zsh-syntax-highlighting zsh-autosuggestions mix colored-man-pages git shrink-path fzf asdf)/' $ZSHRC
+
+  # Copy .gitconfig template so we can configure it.
+
+  cp ./templates/.gitconfig $HOME
+
+  sed -i "s/#GITEMAIL/$gitemail/" .gitconfig
+  sed -i "s/#GITNAME/$gitname/" .gitconfig
+  sed -i "s@#GITMESSAGE@$HOME/.gitmessage.txt@" .gitconfig
+
 
   print "Creating links for dotfiles"
-  ln -sv $PWD/.gitconfig $HOME
   ln -sv $PWD/templates/.gitmessage.txt $HOME
   ln -sv $PWD/.alias $HOME
   ln -sv $PWD/.env_vars $HOME
@@ -47,6 +97,14 @@ install_linux() {
 
   print "Add  aliases to .zshrc file."
   echo "if [ -f $HOME/.alias ]; then source $HOME.alias; fi" >> $ZSHRC
+
+  if [[ $nvim_consent == "Y" || $nvim_consent == "y" ]]; then
+	  print "Configuring nvim."
+	  configure_nvim
+	  echo "OK."
+  else
+	  echo "Skipping configuring nvim."
+  fi
 
 }
 
