12f530514d23413c5119a4dc1f9482d1208af97a
stefan <stefanvandelft93@gmail.com>
2023-08-27 20:22:59 +0200

    Also set installer in install_programs file.

--- install_programs.sh
+++ install_programs.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+if [ -x "$(command -v apt)" ]; then
+    echo "package-manager: apt"
+    export PACKAGE_MANAGER="apt"
+    export INSTALL_FLAGS="install"
+elif [ -x "$(command -v pacman)" ]; then
+    echo "package-manager: pacman"
+    export PACKAGE_MANAGER="pacman"
+    export INSTALL_FLAGS="-S --noconfirm"
+else
+    echo "Unsupported package manager"
+    exit 1
+fi
+
 # Create a directory to store repositories
 mkdir $HOME/repos
 
