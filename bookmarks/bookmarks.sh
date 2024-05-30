4686a999ebda76634dbd95475f5b8c23f26c9aa7
stefan <stefanvandelft93@gmail.com>
2024-05-30 15:49:10 +0200

    add bookmarks tool using rofi

--- bookmarks/bookmarks.sh
+++ bookmarks/bookmarks.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env sh
 
+bookmarks=~/dotfiles/bookmarks/bookmarks.sh
 websites=~/dotfiles/bookmarks/websites
+edit=~/dotfiles/bookmarks/edit
 
+#TODO command bookmars? docs from org / obsidian? / greenclip
 case "$1" in
     websites)
         cat -n $websites \
             | rofi -dmenu \
             | cut -d \; -f 2 \
-            | xargs -r firefox && swaymsg workspace 10
+            | (xargs -r -0 firefox && swaymsg workspace 10)
 
         ;;
 
+    edit)
+        cat -n $edit \
+            | rofi -dmenu \
+            | cut -d \; -f 2 \
+            | xargs -r -0 -I % sh -c 'emacsclient -e "(+workspace/new)" | emacsclient -n %' && swaymsg workspace 1
+
+        ;;
     *)
-        printf "websites" | rofi -dmenu | xargs ./bookmarks.sh
+        printf "websites" | rofi -dmenu | xargs $bookmarks
 
         ;;
 
