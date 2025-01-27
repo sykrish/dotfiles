306700305a3da9fa0b0fc6f56aafd6063efb57ce
stefan <stefanvandelft93@gmail.com>
2025-01-27 15:36:58 +0100

    install other fzf plugin for zsh

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -40,12 +40,13 @@ configure_zsh() {
   print "...and its plugins!"
   git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
 
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf
   debug_stop
 
   print "... enabling plugins."
-  sed -i 's/plugins=(git)/plugins=(zsh-syntax-highlighting zsh-autosuggestions mix colored-man-pages git shrink-path fzf asdf)/' $ZSHRC
+  sed -i 's/plugins=(git)/plugins=(zsh-syntax-highlighting zsh-autosuggestions mix colored-man-pages git shrink-path fzf-zsh-plugin asdf)/' $ZSHRC
   debug_stop
 
   print "Append settings to .zsh file"
