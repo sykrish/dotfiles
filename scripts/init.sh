8c2f3a4df832966cd520523414ad68ef72be5c33
stefan <stefanvandelft93@gmail.com>
2025-01-27 15:35:48 +0100

    make apt-get more quite

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,9 +2,9 @@
 
 # Detect the package manager and set the environment variable
 if [ -x "$(command -v apt)" ]; then
-    echo "package-manager: apt"
+    echo "package-manager: apt-get"
     export PACKAGE_MANAGER="apt-get"
-    export INSTALL_FLAGS="-q install"
+    export INSTALL_FLAGS="-q=2 install"
 elif [ -x "$(command -v pacman)" ]; then
     echo "package-manager: pacman"
     export PACKAGE_MANAGER="pacman"
