70161fd26b87f568776ef34c71689143c5ec9514
stefan <stefanvandelft93@gmail.com>
2025-04-13 23:12:14 +0200

    install asdf bin and manually add to path

--- scripts/install.sh
+++ scripts/install.sh
@@ -100,18 +100,26 @@ install_asdf_plugin() {
 install_asdf() {
   info "Install asdf"
 
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.16.0 1>/dev/null
+  # download
+  file=$(wget -q -O- https://github.com/asdf-vm/asdf/releases/download/v0.16.7/asdf-v0.16.7-linux-amd64.tar.gz | tar xvz)
+  mkdir $HOME/bin
+  cp file $HOME/bin
 
-  echo '. "$HOME/.asdf/asdf.sh"' >>$ZSHRC
+  # update path for running shell
+  export PATH="$HOME/bin:$PATH"
+
+  # echo '. "$HOME/.asdf/asdf.sh"' >>$ZSHRC
+  'export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"' >>$ZSHRC
   print "Source $ZSHRC so that asdf works"
   if [[ "$SHELL" == *zsh ]]; then
     source $ZSHRC
-    source $HOME/.asdf/asdf.sh
   else
     . $ZSHRC
-    . $HOME/.asdf/asdf.sh
   fi
   source $ZSHRC
+
+  mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
+  asdf completion zsh >"${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
 
   print "Install asdf done"
   debug_stop
