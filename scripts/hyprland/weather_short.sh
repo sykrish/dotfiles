cb04599525c6f9a60afdd478a0c8b6bb471f2034
Stefan van Delft <stefanvandelft93@gmail.com>
2025-09-06 19:35:41 +0200

    make weather script more robust

--- scripts/hyprland/weather_short.sh
+++ scripts/hyprland/weather_short.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env sh
 
-curl -s https://wttr.in/Haarlem\?format="%t\n"
+FILE=/tmp/weather_short
+
+request() {
+    WEATHER=$(curl -s https://wttr.in/Haarlem\?format="%t\n")
+    echo $WEATHER > $FILE
+}
+
+get_weather() {
+    if [ -f "$FILE" ]; then
+        DATA=$(cat $FILE)
+
+        if grep -q "please" <<< $DATA ; then
+            : # do nothing
+        else
+            echo $DATA
+        fi
+    fi
+}
+
+case $1 in
+    request)
+        request
+        ;;
+
+    *)
+        get_weather
+        ;;
+esac
